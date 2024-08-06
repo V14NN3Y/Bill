ad4acafbb9f4fef75b98af53e75cb94d724e870d
V14NN3Y <bill.adjagboni@epitech.eu>
2024-08-06 01:04:58 +0100

    add

---
+++ neutrinos.h
@@ -0,0 +1,22 @@
+/*
+** EPITECH PROJECT, 2024
+** neutrinos,h
+** File description:
+** header file
+*/
+
+#ifndef NEUTRINOS_H
+    #define NEUTRINOS_H
+    #include <string.h>
+    #include <unistd.h>
+    #include <stdlib.h>
+    #include <math.h>
+    #include <stdio.h>
+typedef struct neutrinos {
+    double nb;
+    double ar_mean;
+    double har_mean;
+    double sd;
+} neutrinos_t;
+
+#endif
